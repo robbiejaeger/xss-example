8e382998ea978822b491dbdfe4328763b2b77247
Robbie Jaeger <jaeger.rob@gmail.com>
2018-02-09 10:33:24 -0700

    Add post request to add penguin species with front end fetch request

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,24 @@
-console.log('Hi!');
+$('#wish-submit').on('click', () => {
+  const inputText = $('#wish-input').val();
+
+  addItemtoDB(inputText);
+});
+
+const addItemtoDB = (itemData) => {
+  fetch('api/v1/penguins', {
+    method: 'POST',
+    body: JSON.stringify({ species: itemData }),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  })
+  .then(response => {
+    return response.json();
+  })
+  .then(parsedResponse => {
+    console.log(parsedResponse);
+  })
+  .catch(err => {
+    console.error('Error submitting wish:', err);
+  })
+};
--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 
 app.locals.penguins = [{species: 'gentoo'}, {species: 'adelie'}];
 
+app.use(bodyParser.json());
+
 app.get('/api/v1/penguins', (request, response) => {
   response.status(200).json(app.locals.penguins);
+});
+
+app.post('/api/v1/penguins', (request, response) => {
+  app.locals.penguins.push({ species: request.body.species });
+  console.log(app.locals.penguins);
+  response.status(201).json(app.locals.penguins);
 });
 
 app.listen(app.get('port'), () => {
